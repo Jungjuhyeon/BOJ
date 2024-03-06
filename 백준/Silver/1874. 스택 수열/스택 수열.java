3edd41653ec64fff256c23121e4deee6386bc156
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2024-03-06 16:02:59 +0900

    [Silver II] Title: 스택 수열, Time: 344 ms, Memory: 27312 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -4,39 +4,32 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        Stack<Integer> stack = new Stack<>();
 
         int N = Integer.parseInt(br.readLine());
+        int now =1;
+        Stack<Integer> stack = new Stack<>();
 
-        int start =0;
-
-        for(int j=0;j<N;j++){
-
-            int value = Integer.parseInt(br.readLine());
-
-            if (value > start) {
-                for (int i = start + 1; i <= value; i++) {
-                    stack.push(i);
-                    sb.append("+").append('\n');
-                }
-                start = value;
+        for(int i =0; i<N;i++){
+            int num = Integer.parseInt(br.readLine());
+            while (now<=num){
+                stack.push(now);
+                sb.append('+').append('\n');
+                now++;
             }
-            else if(stack.peek() != value){
-                System.out.println("NO");
-                return;
+            if(stack.peek() == num){
+                stack.pop();
+                sb.append('-').append('\n');
+            }else{
+                sb.setLength(0);
+                sb.append("NO");
+                break;
             }
-
-            stack.pop();
-            sb.append('-').append('\n');
         }
-
         System.out.println(sb);
-
-
-
     }
 }
