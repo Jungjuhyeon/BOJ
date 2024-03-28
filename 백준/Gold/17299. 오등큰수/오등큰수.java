6e706b4bcb50e7b175d750f54ab5fbafaeceb2a9
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2024-03-28 23:53:17 +0900

    [Gold III] Title: 오등큰수, Time: 1000 ms, Memory: 141912 KB -BaekjoonHub

--- 백준/Gold/17299. 오등큰수/오등큰수.java
+++ 백준/Gold/17299. 오등큰수/오등큰수.java
@@ -2,50 +2,42 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-
-        String[] strinput = br.readLine().split(" ");
-        int[] input = new int[N]; //초반에 숫자 입력한 값
-
-        //초반 숫자 배열
-        for (int i = 0; i < N; i++) {
-            input[i] = Integer.parseInt(strinput[i]);
-        }
-
-        //숫자 횟수를 위한 배열
-        int[] num_count = new int[1000001];
-        for (int i = 0; i < N; i++) {
-            num_count[input[i]] += 1;
-        }
-
-        //결과값으 넣을 배열
-        int[] result = new int[N];
-
+        StringBuilder sb = new StringBuilder();
         Stack<Integer> stack = new Stack<>();
 
-        for (int i = 0; i < N; i++) {
-            while (!stack.isEmpty() && num_count[input[stack.peek()]] < num_count[input[i]]) {
-                result[stack.pop()] = input[i];
-            }
-            stack.push(i);
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-
-        }
-        while (!stack.isEmpty()){
-            result[stack.pop()] = -1;
-          }
-
-        StringBuilder sb = new StringBuilder();
+        int[] in = new int[N];
+        int[] count = new  int[1000001];
+        int[] anw = new int[N];
 
         for(int i=0; i<N;i++){
-            sb.append(result[i]).append(' ');
+            in[i] = Integer.parseInt(st.nextToken());
+            count[in[i]]++;
         }
+
+        for(int i=0; i<N; i++) {
+            while (!stack.isEmpty() && count[in[stack.peek()]] < count[in[i]]){
+                anw[stack.pop()] = in[i];
+            }
+            stack.push(i);
+        }
+
+        while(!stack.isEmpty()){
+            anw[stack.pop()] = -1;
+        }
+
+        for(int i =0; i<N; i++){
+            sb.append(anw[i]).append(" ");
+        }
+
         System.out.println(sb);
 
     }
