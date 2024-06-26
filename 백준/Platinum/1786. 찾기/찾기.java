96af09e203481e03f8f442c7fea04ac8e3607fd2
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2024-06-26 10:02:31 +0900

    [Platinum V] Title: 찾기, Time: 2816 ms, Memory: 91124 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.java
+++ 백준/Platinum/1786. 찾기/찾기.java
@@ -7,17 +7,16 @@ public class Main {
     static int[] F;
     static List<Integer> ans =new ArrayList<>();
 
-
     static void makeTable(){
-        int n = p.length();
 
-        int idx =0;
+        int idx =0; // 패턴의 현재 위치를 가리키는 인덱스
 
-        for(int i=1; i<n; i++){
+        for(int i=1; i<p.length(); i++){
+            // 패턴의 문자가 일치하지 않을 때, idx를 F 값으로 갱신
             while(idx>0 && p.charAt(i)!=p.charAt(idx)){
                 idx = F[idx-1];
             }
-
+            // 패턴의 문자가 일치할 때, idx를 증가시키고 F에 값 설정
             if(p.charAt(i)==p.charAt(idx)){
                 F[i]=++idx;
             }
@@ -26,16 +25,18 @@ public class Main {
     static void solve(){
 
         int idx = 0;
+
         for(int i=0; i<t.length(); i++){
-            while(idx>0 && t.charAt(i)!=p.charAt(idx)){
+
+            while(idx>0 && t.charAt(i)!=p.charAt(idx)){// 텍스트와 패턴의 문자가 일치하지 않을 때 idx를 F 값으로 갱신
                 idx = F[idx-1];
             }
-            if(t.charAt(i)==p.charAt(idx)) {
+            if(t.charAt(i)==p.charAt(idx)) { // 텍스트와 패턴의 문자가 일치할 때
                 if (idx == p.length() - 1) { //패턴의 끝에 도착했을떄,
                     ans.add(i-idx+1);
-                    idx = F[idx];
+                    idx = F[idx]; // idx를 실패 F 값으로 갱신
                 } else {
-                    idx += 1;
+                    idx += 1; // idx를 증가시켜 계속 탐색
                 }
             }
         }
