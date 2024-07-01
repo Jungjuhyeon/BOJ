ec421f0d3f328dca81c8cb8d391d6f5e4e57cc69
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2024-07-01 16:34:01 +0900

    [Silver II] Title: DFS와 BFS, Time: 208 ms, Memory: 20000 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,86 +1,74 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static List<Integer>[] list ;
+    static boolean[] v;
+    static StringBuilder sb =new StringBuilder();
 
-    static int node;
-    static int line;
-    static int start;
-    static int[][] arg;
-    static boolean[] check;
-
-    static StringBuilder sb;
-
-    static Queue<Integer> queue = new LinkedList<>();
-
-
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        sb = new StringBuilder();
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        node = Integer.parseInt(st.nextToken());
-        line = Integer.parseInt(st.nextToken());
-        start = Integer.parseInt(st.nextToken());
-
-        arg = new int[node+1][node+1];
-        check = new boolean[node+1];
-
-        for(int i =0;i<line;i++){
-            StringTokenizer str = new StringTokenizer(br.readLine());
-
-            int a = Integer.parseInt(str.nextToken());
-            int b = Integer.parseInt(str.nextToken());
-
-            arg[a][b] =1;
-            arg[b][a] =1;
-
+    static void dfs(int i){
+        v[i] = true;
+        sb.append(i).append(" ");
+//        System.out.print(i);
+        for(int j : list[i]) { //0->N
+            if (!v[j]) {
+                dfs(j);
+            }
         }
+    }
 
-        dfs(start);
+    static void bfs(int i){
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        v[i] = true;
+        q.offer(i);
+        while (!q.isEmpty()) {
+            i = q.poll();
+            sb.append(i).append(" ");
+            for (int j : list[i]) { //0 -> N
+                if (!v[j]) {
+                    v[j] = true;
+                    q.offer(j);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st ;
+
+        st = new StringTokenizer(br.readLine()," ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        list = new List[N+1];
+        for(int i=1;i<=N;i++){
+            list[i] = new ArrayList<>();
+        }
+        v = new boolean[N+1];
+        for(int i=0;i<M;i++){
+            st = new StringTokenizer(br.readLine()," ");
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            list[from].add(to);
+            list[to].add(from);
+        }
+//        for (List<Integer> a : list) System.out.println(a);
+
+        // 각 리스트를 정렬하여 작은 번호부터 방문하도록 설정
+        for (int i = 1; i <= N; i++) {
+            list[i].sort((a, b) -> Integer.compare(a, b));
+        }
+        
+        dfs(V);
         sb.append("\n");
-
-        check = new boolean[node+1];
-        bfs(start);
+        v = new boolean[N+1];
+        bfs(V);
 
         System.out.println(sb);
 
     }
 
-    public static void dfs(int start){
-        check[start] = true;
-        sb.append(start).append(" ");
-
-        for(int i =0 ;i <= node;i++){
-            if(arg[start][i] ==1 && !check[i]){
-                check[i] =true;
-                dfs(i);
-            }
-        }
-    }
-
-    public static void bfs(int start){
-        queue.add(start);
-        check[start] = true;
-
-        while(!queue.isEmpty()){
-            start = queue.poll();
-            sb.append(start).append(" ");
-
-            for(int i =0; i<=node;i++){
-                if(arg[start][i] ==1 && !check[i]){
-                    check[i] =true;
-                    queue.add(i);
-                }
-            }
-
-        }
-
-    }
 }
