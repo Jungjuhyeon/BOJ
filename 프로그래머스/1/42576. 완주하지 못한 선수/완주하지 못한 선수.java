386c7c00e6ce647e56d3bfba6ccad9cdf1ca4047
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2024-07-08 20:26:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 67.99 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,13 +1,25 @@
+import java.util.*;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        String answer = "";
         
-        for(int i=0;i<participant.length;i++){
-            if(completion.contains(participant[i])){
-                completion.r
-            }
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(String player : participant){
+            map.put(player, map.getOrDefault(player, 0) + 1);
         }
         
-        String answer = "";
+        for(String player : completion ){
+            map.put(player, map.get(player) - 1);
+        }
+        
+        for(String key : map.keySet()) {
+            if (map.get(key) != 0){
+                answer = key;
+                break;
+            }
+        }
         return answer;
     }
 }
