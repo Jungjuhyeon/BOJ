16d77838fa1c2a1c6f77ac4e1c01577a72c2b5a2
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2024-07-10 17:28:24 +0900

    [level 2] Title: 의상, Time: 0.06 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        
+        HashMap<String,Integer> map= new HashMap<>();
+        
+        for(String[] clothe : clothes){
+            String type = clothe[1];
+            map.put(type,map.getOrDefault(type,0)+1);
+        }
+        
+        int answer =1;
+        
+        for(String key : map.keySet()){
+            answer *= (map.get(key)+1);
+        }
+        
+        answer -=1;
+        
+        return answer;
+    }
+}
