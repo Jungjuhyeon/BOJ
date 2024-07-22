32c0fa1a518f1285e8a72aede3cf2f7bfa69514c
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2024-07-22 20:53:02 +0900

    [Gold V] Title: 집합의 표현, Time: 436 ms, Memory: 49980 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -8,7 +8,7 @@ public class Main {
 
     static int find(int i){
         if(parent[i] ==i) return i;
-        return find(parent[i]);
+        return parent[i] = find(parent[i]);
     }
 
     static void union(int a,int b){
