c176e134fbd6183dd2aaf326962a652c2519b5f1
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2024-07-28 17:52:20 +0900

    [level 2] Title: 더 맵게, Time: 1418.66 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -7,17 +7,17 @@ class Solution {
         int answer =0;
         
         for(int i : scoville){
-            pq.offer(i);
+            pq.add(i);
         }
         
         while(pq.peek()<K){
-            if(pq.size() ==1){
+            if(pq.size() ==1){ //만약 처음값이 K보다 작은데, pq에 1개만 있다면, 불가능하므로 -1을 리턴함.
                 return -1;
             }
             int a = pq.poll(); //첫번쨰값
             int b = pq.poll(); //두번째값
             int mix = a + (b*2);
-            pq.offer(mix);
+            pq.add(mix);
             answer++;
         }
         
