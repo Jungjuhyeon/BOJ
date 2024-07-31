8b6215b1aed2b25cf289de9389a941b91874db3f
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2024-07-31 16:47:30 +0900

    [level 3] Title: 이중우선순위큐, Time: 65.00 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,38 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(String[] operations) {
-        int[] answer = new int[2];
-        StringTokenizer st;
-        
-        PriorityQueue<Integer> minpq = new PriorityQueue<>();
-        PriorityQueue<Integer> maxpq = new PriorityQueue<>(Collections.reverseOrder());
-        
-        int length = operations.length;
-        
-        for(int i=0;i<length;i++){
-            st = new StringTokenizer(operations[i]," ");
-            String text = st.nextToken();
-            int num = Integer.parseInt(st.nextToken()); 
-            if(text.equals("I")){
-                minpq.offer(num);
-                maxpq.offer(num);
-            }
-            else{
-            if(num ==-1){
-                if(!minpq.isEmpty()){
-                    maxpq.remove(minpq.poll());
-                }
-            }else{
-                if(!maxpq.isEmpty()){
-                    minpq.remove(maxpq.poll());
-                    }
-                }
-            }
-        }
-        if(!maxpq.isEmpty()) answer[0] = maxpq.poll();
-        if(!minpq.isEmpty()) answer[1] = minpq.poll();
-    
+        int[] answer = {};
         return answer;
     }
 }
