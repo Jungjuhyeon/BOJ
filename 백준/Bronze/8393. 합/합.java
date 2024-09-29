22f0384bdb63c7797bd69fb844f98c32e5cc8cad
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2024-09-29 23:24:51 +0900

    [Bronze V] Title: 합, Time: 100 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        int sum =0;
+        for(int i=1;i<=N;i++){
+            sum +=i;
+        }
+        br.close();
+        System.out.println(sum);
+        
+    }
+}
