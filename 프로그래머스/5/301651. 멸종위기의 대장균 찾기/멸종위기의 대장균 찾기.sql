bfaead5fd25efd42f8178b7bb33e4ad6111a144d
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2025-03-01 18:26:50 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -0,0 +1,20 @@
+WITH RECURSIVE rc AS(
+    SELECT id,parent_id,1 AS gen
+    FROM ECOLI_DATA
+    WHERE PARENT_ID IS NULL
+    
+    UNION ALL
+
+    SELECT e.id, e.parent_id, rc.gen+1 AS gen
+    FROM ECOLI_DATA AS e
+    JOIN rc ON rc.id = e.parent_id
+)
+# SELECT * FROM rc;
+
+SELECT COUNT(r1.gen) AS COUNT,
+        r1.gen AS GENERATION
+FROM rc AS r1 LEFT JOIN rc AS r2
+ON r1.id = r2.parent_id
+WHERE r2.parent_id IS NULL
+GROUP BY r1.gen
+ORDER BY r1.gen
