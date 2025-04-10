21aa080d79d26d146544c267805b6ea47f413d8a
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2025-04-10 20:35:37 +0900

    [Bronze II] Title: 나머지, Time: 100 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.io.*;
+ 
+public class Main {
+    public static void main(String[] args) throws IOException {
+ 
+        boolean[] arr = new boolean[42];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        for(int i = 0 ; i < 10 ; i++) {
+            arr[Integer.parseInt(br.readLine()) % 42] = true;
+        }
+        
+        int count = 0;
+        for(boolean value : arr) {
+            if(value){    // value 가 true 라면
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
