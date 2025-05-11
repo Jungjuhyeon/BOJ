5e58c50c7d456d7092b1a89f9478a97a666afe97
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2025-05-12 00:10:40 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,12 +1,29 @@
-SELECT
-    DISTINCT(id),
-    email,
-    first_name,
-    last_name
-FROM
-    developers d
-    JOIN skillcodes s
-    ON s.name IN ('C#', 'Python')
-    AND d.skill_code & s.code = s.code 
-ORDER BY
-    1
+SELECT DISTINCT
+        ID,
+        EMAIL,
+        FIRST_NAME,
+        LAST_NAME
+FROM DEVELOPERS as d
+JOIN SKILLCODES as s
+ON d.SKILL_CODE & s.CODE = s.Code
+WHERE s.NAME = 'Python' OR s.NAME = 'C#'
+ORDER BY ID ASC
+
+
+
+
+
+
+
+
+# SELECT
+#     DISTINCT(id),
+#     email,
+#     first_name,
+#     last_name
+# FROM
+#     developers d
+#     JOIN skillcodes s
+#     AND d.skill_code & s.code = s.code 
+# ORDER BY
+#     1
