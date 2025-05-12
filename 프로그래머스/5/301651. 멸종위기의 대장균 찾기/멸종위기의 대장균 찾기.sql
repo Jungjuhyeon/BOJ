33bf56256563376d33f75b5bd684d36b17adcdf5
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2025-05-12 19:23:55 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -1,20 +1,53 @@
-WITH RECURSIVE rc AS(
-    SELECT id,parent_id,1 AS gen
+WITH RECURSIVE rs AS(
+    SELECT ID, PARENT_ID, 1 AS GEN
     FROM ECOLI_DATA
     WHERE PARENT_ID IS NULL
     
-    UNION ALL
+    UNION ALL 
+    
+    SELECT e.ID, e.PARENT_ID, rs.GEN+1 AS GEN
+    FROM ECOLI_DATA as e
+    JOIN rs ON rs.ID = e.PARENT_ID
+) 
 
-    SELECT e.id, e.parent_id, rc.gen+1 AS gen
-    FROM ECOLI_DATA AS e
-    JOIN rc ON rc.id = e.parent_id
-)
-# SELECT * FROM rc;
+SELECT COUNT(*) AS COUNT ,
+        fir.GEN as GENERATION
+FROM rs as fir
+LEFT JOIN rs as second ON fir.ID = second.PARENT_ID
+WHERE second.ID IS NULL
+GROUP BY fir.GEN
 
-SELECT COUNT(r1.gen) AS COUNT,
-        r1.gen AS GENERATION
-FROM rc AS r1 LEFT JOIN rc AS r2
-ON r1.id = r2.parent_id
-WHERE r2.parent_id IS NULL
-GROUP BY r1.gen
-ORDER BY r1.gen
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# WITH RECURSIVE rc AS(
+#     SELECT id,parent_id,1 AS gen
+#     FROM ECOLI_DATA
+#     WHERE PARENT_ID IS NULL
+    
+#     UNION ALL
+
+#     SELECT e.id, e.parent_id, rc.gen+1 AS gen
+#     FROM ECOLI_DATA AS e
+#     JOIN rc ON rc.id = e.parent_id
+# )
+# # SELECT * FROM rc;
+
+# SELECT COUNT(r1.gen) AS COUNT,
+#         r1.gen AS GENERATION
+# FROM rc AS r1 LEFT JOIN rc AS r2
+# ON r1.id = r2.parent_id
+# WHERE r2.parent_id IS NULL
+# GROUP BY r1.gen
+# ORDER BY r1.gen
