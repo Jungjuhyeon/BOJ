f3111b6af666dd75dc1cf8a26d8a060f2cbf1a75
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2025-05-12 19:05:26 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,9 +1,18 @@
-SELECT third.ID
-FROM ECOLI_DATA as first
-LEFT JOIN ECOLI_DATA as second ON first.ID = second.PARENT_ID
-LEFT JOIN ECOLI_DATA as third ON second.ID = third.PARENT_ID
-WHERE first.PARENT_ID IS NULL AND second.ID IS NOT NULL AND third.ID IS NOT NULL
-ORDER BY third.ID ASC
+WITH RECURSIVE rc AS(
+    SELECT ID, PARENT_ID, 1 AS GEN
+    FROM ECOLI_DATA
+    WHERE PARENT_ID IS NULL
+    
+    UNION ALL 
+    
+    SELECT e.ID, e.PARENT_ID, rc.GEN+1 AS GEN
+    FROM ECOLI_DATA as e
+    JOIN rc ON rc.ID = e.PARENT_ID
+)
+
+SELECT ID
+FROM rc
+WHERE GEN = 3
 
 
 
@@ -20,9 +29,24 @@ ORDER BY third.ID ASC
 
 
 
-# SELECT third.ID AS ID
-# FROM ECOLI_DATA AS fir
-# LEFT JOIN ECOLI_DATA AS second ON fir.ID = second.PARENT_ID
-# INNER JOIN ECOLI_DATA AS third ON second.ID = third.PARENT_ID
-# WHERE fir.PARENT_ID IS NULL AND second.ID IS NOT NULL
-# ORDER BY ID
+
+
+
+# SELECT third.ID
+# FROM ECOLI_DATA as first
+# LEFT JOIN ECOLI_DATA as second ON first.ID = second.PARENT_ID
+# LEFT JOIN ECOLI_DATA as third ON second.ID = third.PARENT_ID
+# WHERE first.PARENT_ID IS NULL AND second.ID IS NOT NULL AND third.ID IS NOT NULL
+# ORDER BY third.ID ASC
+
+
+
+
+
+
+
+
+
+
+
+
