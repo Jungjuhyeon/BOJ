c238c46968f3b0804822cc1bb111fc00880ed3f3
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2025-11-01 18:36:05 +0900

    [level 3] Title: 네트워크, Time: 0.42 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,29 +1,45 @@
 import java.util.*;
 
 class Solution {
-    static boolean[] v;
+    static int[] parent;
     public int solution(int n, int[][] computers) {
-        v = new boolean[n];
         
-        int answer =0;
+        parent = new int[n];
         
+        //부모 초기화
         for(int i=0;i<n;i++){
-            if(!v[i]){
-                answer++;
-                dfs(i,n,computers);
-            }
+            parent[i] = i;
         }
-        return answer;
+        
+        for(int i=0 ; i<n;i++){
+            for(int j=0;j<n;j++)
+                {
+                if(i != j && computers[i][j] ==1){
+                    union(i,j);
+                    }
+                }
+            }
+        
+        Set<Integer> set = new HashSet<>();
+        
+        for(int i : parent){
+            set.add(find(i)); // ← 핵심 수정
+        }
+        return set.size();
+        
     }
     
-    public static void dfs(int i,int n,int[][] computers){
-        v[i] = true;
-        
-        for(int j=0;j<n;j++){
-            if(computers[i][j] == 1 && !v[j]){
-                dfs(j,n,computers);
-            }
+    public static void union(int a,int b){
+        int aRoot = find(a);
+        int bRoot = find(b);
+        if(aRoot != bRoot){
+            parent[bRoot] = aRoot;
         }
-        
+    }
+    public static int find(int i){
+        if(parent[i] == i )return i;
+        else{
+            return parent[i] = find(parent[i]);
+        }
     }
 }
