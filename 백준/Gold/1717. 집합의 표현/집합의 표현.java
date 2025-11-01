dca6f11794a7cad3151cd168a4655abc13cc3205
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2025-11-01 19:12:57 +0900

    [Gold V] Title: 집합의 표현, Time: 416 ms, Memory: 53952 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,46 +1,35 @@
-import java.io.*;
+
 import java.util.*;
-
-
+import java.io.*;
 public class Main {
-    static int N,M;
-    static int[] parent;
-
-    static int find(int i){
-        if(parent[i] ==i) return i;
-        return parent[i] = find(parent[i]);
-    }
-
-    static void union(int a,int b){
-        int aRoot = find(a);
-        int bRoot = find(b);
-        parent[bRoot] = aRoot;
-    }
-
-    public static void main(String[] args)throws Exception {
+    static int[] p;
+    public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        StringBuilder sb =new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 
         st = new StringTokenizer(br.readLine()," ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        parent = new int[N+1];
+        p = new int[n+1];
 
-        for(int i=0;i<N+1;i++){
-            parent[i] = i;
+        for(int i=0;i<=n;i++){
+            p[i] = i;
         }
 
-        for(int i=0;i<M;i++){
+        for(int i=0;i<m;i++){
             st = new StringTokenizer(br.readLine()," ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
             if(a == 0){
                 union(b,c);
-            }else{
-                if(find(b)==find(c)){
+            }
+            if(a == 1){
+                int bRoot = find(b);
+                int cRoot = find(c);
+                if(bRoot ==cRoot){
                     sb.append("YES").append("\n");
                 }else{
                     sb.append("NO").append("\n");
@@ -49,10 +38,19 @@ public class Main {
 
         }
 
-        br.close();
-
         System.out.println(sb);
+    }
 
-
+    public static int find(int i){
+        if(p[i] == i )return i;
+        else{
+            return p[i] = find(p[i]);
+        }
+    }
+    public static void union(int a,int b){
+        int aRoot = find(a);
+        int bRoot = find(b);
+        p[bRoot] = aRoot;
     }
 }
+
