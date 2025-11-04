7062ffae1edcdfe335e2da07393ceb97f41d7832
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2025-11-04 14:39:14 +0900

    [level 3] Title: 여행경로, Time: 51.79 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,34 +1,47 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    ArrayList<String> list = new ArrayList<>();
-    boolean[] v;
-    int length;
+    static boolean[] v;
+    static int R;
+    static List<String[]> list;
     public String[] solution(String[][] tickets) {
-        length = tickets.length;
-        v = new boolean[length];
+        list = new ArrayList<>();
+        R = tickets.length;
         
-        perm(0,"ICN","ICN",tickets);
+        for(int i=0 ; i<tickets.length ; i++){
+            v = new boolean[tickets.length];
+            if(tickets[i][0].equals("ICN")){
+                v[i] = true;
+                String[] path = new String[tickets.length + 1];
+                path[0] = "ICN";
+                dfs(i, tickets, path, 1); 
+            }
+        }
         
-        Collections.sort(list);
-                
-        return list.get(0).split(" ");
+        
+        // 사전순으로 정렬
+        list.sort((a, b) -> Arrays.compare(a,b));
+
+        return list.get(0); // 가장 앞선 경로 반환
     }
     
-    public void perm(int depth,String start, String path,String[][] tickets){
-        if(depth == length){
-            list.add(path);
+    public void dfs(int idx, String[][] tickets, String[] path, int depth) {
+        path[depth] = tickets[idx][1];
+
+        if(depth ==R){
+            list.add(path.clone());
             return;
         }
-        for(int i=0;i<length;i++){
-            if(tickets[i][0].equals(start)){
-                if(v[i]) continue;
+        
+        for (int i = 0; i < tickets.length; i++) {
+            if (!v[i] && tickets[idx][1].equals(tickets[i][0])) {
                 v[i] = true;
-                perm(depth+1,tickets[i][1],path +" "+ tickets[i][1],tickets);
+                dfs(i, tickets, path, depth + 1);
                 v[i] = false;
             }
         }
+       
+        
+        
     }
 }
- 
