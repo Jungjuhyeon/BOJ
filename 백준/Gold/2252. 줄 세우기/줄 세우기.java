a16b9410e53cc2c926ce0c8b97ddcb2ac055e901
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2025-11-15 00:42:24 +0900

    [Gold III] Title: 줄 세우기, Time: 416 ms, Memory: 47380 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,52 +1,51 @@
-import java.io.*;
+
 import java.util.*;
+import java.io.*;
 
 public class Main {
     static int N,M;
     static List<Integer>[] g;
-    static int[] indegree;
-    static StringBuilder sb =new StringBuilder();
+    static int[] indegree; //진입차수 메모
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        st = new StringTokenizer(br.readLine());
-
+        st = new StringTokenizer(br.readLine()," ");
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        g = new ArrayList[N+1];
 
-        g= new List[N+1];
-
-        for(int i=1;i<N+1;i++){
+        for(int i=0;i<N+1;i++){
             g[i] = new ArrayList<>();
         }
-
-        indegree =new int[N+1];
+        indegree = new int[N+1];
 
         for(int i=0;i<M;i++){
-            st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine()," ");
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
             g[from].add(to);
             indegree[to]++;
         }
-
         bfs();
-        System.out.print(sb);
+
+        System.out.println(sb);
     }
 
-    static void bfs(){
+    public static void bfs(){
         ArrayDeque<Integer> q = new ArrayDeque<>();
-        for(int i=1;i<N+1;i++){
-            if(indegree[i]==0) q.offer(i);
+        for(int i=1;i<=N;i++){
+            if(indegree[i] ==0 ) q.offer(i);
         }
+
         while(!q.isEmpty()){
-            int i = q.poll();
+            int i= q.poll();
             sb.append(i).append(" ");
-            for (int j : g[i]) { //0 -> N
+            for(int j : g[i]){
                 indegree[j]--;
-                if (indegree[j]==0) {
+                if(indegree[j] ==0){
                     q.offer(j);
                 }
             }
