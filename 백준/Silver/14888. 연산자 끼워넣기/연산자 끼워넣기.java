e434ded7933df1f01f20c6d0b86270fa9ceb1b3d
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2025-11-15 23:31:08 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 512 ms, Memory: 16428 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,95 +1,65 @@
 
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N,R;
-    static int[] operator;
-    static char[] new_operator,b;
-    static int max = Integer.MIN_VALUE;
+    static int N;
+    static int[] a,yun,b;
     static int min = Integer.MAX_VALUE;
-    static int[] ia;
+    static int max = Integer.MIN_VALUE;
     static boolean[] v;
-
-    static int sum =0;
-
-    static void recur(int cnt) {
-        if(cnt == R){
-            sum = ia[0];
-            for(int i=0; i<R;i++){
-                if(b[i] == '+'){
-                    sum = sum + ia[i+1];
-                }
-                else if(b[i] == '-'){
-                    sum = sum - ia[i+1];
-                }
-                else if(b[i] == '*'){
-                    sum = sum * ia[i+1];
-                }else {
-                    sum = sum / ia[i+1];
-                }
-            }
-            if(max<sum) max =sum;
-            if(min>sum) min = sum;
-            return ;
-        }
-        for(int i=0;i<R;i++){
-            if(v[i]) continue;
-            v[i]= true;
-            b[cnt] = new_operator[i];
-            recur(cnt+1);
-            v[i] = false; //중복 체크 안하면 중복 순열
-        }
-    }
-
-
-
-
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
-        R= N-1;
-        ia = new int[N];
-        operator = new int[4];
-        new_operator = new char[R];
-        b = new char[R];
-
-
-        st = new StringTokenizer(br.readLine());
-
-        for(int i=0;i<N; i++){
-            ia[i] = Integer.parseInt(st.nextToken());
-        }
-        st = new StringTokenizer(br.readLine());
-
-        for(int i=0;i<4; i++) {
-            operator[i] = Integer.parseInt(st.nextToken());
+        a = new int[N];
+        st = new StringTokenizer(br.readLine()," ");
+        for(int i=0;i<N;i++){
+            a[i] = Integer.parseInt(st.nextToken());
         }
 
-        int index =0;
-        for(int i=0; i<operator[0];i++){
-            new_operator[index++] = '+';
-        }
-        for(int i=0; i<operator[1];i++){
-            new_operator[index++] = '-';
-        }
-        for(int i=0; i<operator[2];i++){
-            new_operator[index++] = '*';
-        }
-        for(int i=0; i<operator[3];i++){
-            new_operator[index++] = '%';
+        yun = new int[N-1];
+        st = new StringTokenizer(br.readLine()," ");
+        int idx =0;
+        for(int i=0;i<4;i++){
+            int num = Integer.parseInt(st.nextToken());
+            for(int j=0;j<num;j++){
+                yun[idx++] = i;
+            }
         }
 
-        v = new boolean[R]; //false,false 초기상태
+        b = new int[N-1];
+        v = new boolean[N-1];
 
-//        System.out.println(Arrays.toString(new_operator));
-
-        recur(0);
-
+        perm(0);
         System.out.println(max);
-        System.out.print(min);
+        System.out.println(min);
+
+    }
+    public static void perm(int cnt){
+        if(cnt == N-1){
+            int result = a[0];
+            for(int i=0;i<N-1;i++){
+                int op = b[i];
+                int num = a[i+1];
+                if(op == 0) result +=num;
+                else if(op==1) result -= num;
+                else if(op==2) result *= num;
+                else result/=num;
+            }
+            min = Math.min(min,result);
+            max = Math.max(max,result);
+            return;
+        }
+
+        for(int i=0;i<N-1;i++){
+            if(v[i]) continue;
+            v[i] = true;
+            b[cnt] = yun[i];
+            perm(cnt+1);
+            v[i] = false;
+        }
 
     }
 }
