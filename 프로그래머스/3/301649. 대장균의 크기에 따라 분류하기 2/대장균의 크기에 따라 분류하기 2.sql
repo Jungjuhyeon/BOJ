64b60b3e7b69f3d355b0400497d6627a91e6e320
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2025-11-24 01:07:20 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,17 +1,17 @@
 SELECT ID,
-        CASE
-        WHEN size = 1 THEN 'CRITICAL'
-        WHEN size = 2 THEN 'HIGH'
-        WHEN size = 3 THEN 'MEDIUM'
-        ELSE 'LOW'
-        END AS COLONY_NAME
+       CASE
+       WHEN SIZE = 1 THEN 'CRITICAL'
+       WHEN SIZE = 2 THEN 'HIGH'
+       WHEN SIZE = 3 THEN 'MEDIUM'
+       ELSE 'LOW'
+       END AS COLONY_NAME
 FROM (
-    SELECT ID,
-            # SIZE_OF_COLONY,
-            NTILE(4) OVER(ORDER BY SIZE_OF_COLONY DESC) AS size
+    SELECT
+    ID,
+    NTILE(4) OVER(ORDER BY SIZE_OF_COLONY DESC) AS SIZE
     FROM ECOLI_DATA
-) as ranked
-ORDER BY ID ASC
+) AS p
+ORDER BY ID
 
 
 
@@ -24,19 +24,16 @@ ORDER BY ID ASC
 
 
 
-
-
-
-# SELECT ID, 
-#         CASE WHEN SizeRank = 1 THEN 'CRITICAL'
-#              WHEN SizeRank = 2 THEN 'HIGH'
-#              WHEN SizeRank = 3 THEN 'MEDIUM'
-#              ELSE 'LOW'
-#              END AS COLONY_NAME
-# FROM (
-#     SELECT ID,
-#             SIZE_OF_COLONY,
-#             NTILE(4) OVER(ORDER BY SIZE_OF_COLONY DESC) AS SizeRank
-#     FROM ECOLI_DATA
-# )AS rankDATA
-# ORDER BY ID ASC;
+# # SELECT ID, 
+# #         CASE WHEN SizeRank = 1 THEN 'CRITICAL'
+# #              WHEN SizeRank = 2 THEN 'HIGH'
+# #              WHEN SizeRank = 3 THEN 'MEDIUM'
+# #              ELSE 'LOW'
+# #              END AS COLONY_NAME
+# # FROM (
+# #     SELECT ID,
+# #             SIZE_OF_COLONY,
+# #             NTILE(4) OVER(ORDER BY SIZE_OF_COLONY DESC) AS SizeRank
+# #     FROM ECOLI_DATA
+# # )AS rankDATA
+# # ORDER BY ID ASC;
