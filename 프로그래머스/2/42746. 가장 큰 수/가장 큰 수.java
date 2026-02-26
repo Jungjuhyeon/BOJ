d3552d32577e03f91d94b8aa40f4a0b3a5645bcd
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2026-02-26 21:17:28 +0900

    [level 2] Title: 가장 큰 수, Time: 2963.71 ms, Memory: 391 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,68 +2,21 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
+        String[] arg = new String[numbers.length];
         
-        String[] strArr = new String[numbers.length];
-        for(int i=0 ; i<numbers.length ; i++){
-            strArr[i] = String.valueOf(numbers[i]);
+        for(int i=0;i<numbers.length;i++){
+            arg[i] = String.valueOf(numbers[i]);
         }
+        Arrays.sort(arg,(a,b)-> (b+a).compareTo(a+b));
         
-        Arrays.sort(strArr, (a,b) -> (b+a).compareTo(a+b));
+        if(arg[0].equals("0")) return "0";
+
+        String answer = "";
         
-        if(strArr[0].equals("0")) return "0";
-        
-        StringBuilder sb = new StringBuilder();
-        
-        for(String str : strArr){
-            sb.append(str);
-        }
-        return sb.toString();
-        
+        for(int i=0;i<numbers.length;i++){
+            answer+= arg[i];
+        }    
+        return answer;
     }
-    
 }
 
-
-//     static int R;
-//     static boolean[] v;
-//     static HashSet<String> set = new HashSet<>();
-    
-//     public String solution(int[] numbers) {
-        
-//         R = numbers.length;
-//         v = new boolean[R];
-        
-//         perm(0,numbers,"");
-        
-//         List<Long> list = new ArrayList<>();
-        
-//         for(String str: set){
-//             list.add(Long.valueOf(str));
-//         }
-        
-//         Collections.sort(list, (a,b) -> Long.compare(b,a));
-        
-//         String answer = String.valueOf(list.get(0));
-
-//         return answer;
-//     }
-    
-//     public static void perm(int cnt,int[] numbers,String str){
-        
-//         if(cnt == R){
-//             set.add(str);
-//             return;
-//         }
-        
-//         for(int i=0;i<R;i++){
-//             if(v[i]) continue;
-//             v[i] = true;
-//             perm(cnt+1,numbers,str+ String.valueOf(numbers[i]));
-//             v[i] =false;
-//         }
-//     }
-
-
-//조합 -> HashSet
-//HashSet -> Array 정렬(오름차순)
-// Array[0] 정답.
