e9fbce5cbabe79d6726e84ee5dec0f0e9e31a34e
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2026-02-26 17:33:48 +0900

    [Gold IV] Title: 전화번호 목록, Time: 392 ms, Memory: 191748 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -5,30 +5,43 @@ import java.io.*;
 public class Main {
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+
         int t = Integer.parseInt(br.readLine());
+
         while(t-->0){
+            Trie trie = new Trie();
             int n = Integer.parseInt(br.readLine());
-            String[] arg = new String[n];
-            boolean available = true;
-
+            boolean answer = true;
             for(int i=0;i<n;i++){
-                arg[i] = br.readLine();
-            }
-
-            Arrays.sort(arg);
-            for(int i=0;i<n-1;i++){
-                if(arg[i+1].startsWith(arg[i])){
-                    available = false;
-                    break;
+                String s = br.readLine();
+                if(answer && !trie.insert(s)){
+                    answer = false;
                 }
             }
-            if(available){
-                sb.append("YES").append("\n");
-            }else{
-                sb.append("NO").append("\n");
-            }
+            System.out.println(answer ? "YES" : "NO");
         }
-        System.out.println(sb);
+    }
+
+    static class TrieNode{
+        Map<Character,TrieNode> child = new HashMap<>();
+        boolean isEnd;
+    }
+
+    static class Trie{
+
+        TrieNode root = new TrieNode();
+
+        public boolean insert(String word){
+            TrieNode node = root;
+            for(char c : word.toCharArray()){
+                if(node.isEnd) return false;
+                node = node.child.computeIfAbsent(c,k-> new TrieNode());
+            }
+
+            if(!node.child.isEmpty()) return false;
+            node.isEnd = true;
+            return true;
+        }
+
     }
 }
