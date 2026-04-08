5496728d3bd360809436e89fa0b38d9e0a8b30ad
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2026-04-08 19:09:59 +0900

    [level 1] Title: 신고 결과 받기, Time: 164.45 ms, Memory: 156 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -8,9 +8,9 @@ class Solution {
             map.put(id_list[i],i);
         }
         
-        List<String>[] list = new List[id_list.length];
+        Set<String>[] reportSet = new HashSet[id_list.length];
         for(int i=0;i<id_list.length;i++){
-            list[i] = new ArrayList<>();
+            reportSet[i] = new HashSet<>();
         }
         
         Map<String,Integer> countMap = new HashMap<>();
@@ -20,27 +20,20 @@ class Solution {
             String a = rep[0];
             String b = rep[1];
             int aIdx = map.get(a);
-            if(!list[aIdx].contains(b)){
-                list[aIdx].add(b);
+            if(reportSet[aIdx].add(b)){
                 countMap.put(b,countMap.getOrDefault(b,0)+1);
              };
         }
-        List<String> sinList = new ArrayList();
-        for(String key : countMap.keySet()){
-            if(countMap.get(key)>=k){
-                sinList.add(key);
-            }
-        }
+        
         int[] answer = new int[id_list.length];
         
-        for(int i=0;i<id_list.length;i++){
-            int a =0;
-            for(String s : sinList){
-                if(list[i].contains(s)){
-                    a++;
+        // 결과 계산
+        for (int i = 0; i < id_list.length; i++) {
+            for (String target : reportSet[i]) {
+                if (countMap.getOrDefault(target, 0) >= k) {
+                    answer[i]++;
                 }
             }
-            answer[i]= a;
         }
         
         return answer;
