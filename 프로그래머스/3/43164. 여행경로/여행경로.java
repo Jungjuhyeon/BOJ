02b5de877e279533e56a95e1c154236379258613
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2026-05-11 18:34:06 +0900

    [level 3] Title: 여행경로, Time: 0.40 ms, Memory: 61.7 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,47 +1,54 @@
 import java.util.*;
 
 class Solution {
-    static boolean[] v;
-    static int R;
-    static List<String[]> list;
+    boolean[] v;
+    List<String> answer = new ArrayList<>();
     public String[] solution(String[][] tickets) {
-        list = new ArrayList<>();
-        R = tickets.length;
         
-        for(int i=0 ; i<tickets.length ; i++){
-            v = new boolean[tickets.length];
-            if(tickets[i][0].equals("ICN")){
-                v[i] = true;
-                String[] path = new String[tickets.length + 1];
-                path[0] = "ICN";
-                dfs(i, tickets, path, 1); 
+        v = new boolean[tickets.length];
+        
+        Arrays.sort(tickets, (a,b) -> {
+            if(a[0].equals(b[0])){
+                return a[1].compareTo(b[1]);
             }
-        }
+            return a[0].compareTo(b[0]);
+        });
         
+        List<String> path = new ArrayList<>();
+        path.add("ICN");
         
-        // 사전순으로 정렬
-        list.sort((a, b) -> Arrays.compare(a,b));
-
-        return list.get(0); // 가장 앞선 경로 반환
+        dfs("ICN", "ICN", tickets,path,0);
+        
+        return answer.toArray(new String[0]);
     }
-    
-    public void dfs(int idx, String[][] tickets, String[] path, int depth) {
-        path[depth] = tickets[idx][1];
-
-        if(depth ==R){
-            list.add(path.clone());
-            return;
+    public boolean dfs(String start,String cur,
+                      String[][] tickets,
+                      List<String> path,
+                      int depth){
+        if(depth==tickets.length){
+            answer = new ArrayList<>(path);
+            return true;
         }
         
-        for (int i = 0; i < tickets.length; i++) {
-            if (!v[i] && tickets[idx][1].equals(tickets[i][0])) {
+        for(int i=0;i<tickets.length;i++){
+            
+            String from = tickets[i][0];
+            String to = tickets[i][1];
+            
+            if(v[i]) continue;
+            
+            if(cur.equals(from)){
                 v[i] = true;
-                dfs(i, tickets, path, depth + 1);
+                path.add(to);
+                
+                if(dfs(start, to, tickets, path, depth + 1)) {
+                    return true;
+                }
+                
                 v[i] = false;
+                path.remove(path.size() -1);
             }
         }
-       
-        
-        
+        return false;
     }
 }
