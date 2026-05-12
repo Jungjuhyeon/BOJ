521182e5624838a65b074255138266ed3ee657ea
JUNGJOO <131857282+Jungjuhyeon@users.noreply.github.com>
2026-05-12 09:53:02 +0900

    [level 2] Title: 전화번호 목록, Time: 136.87 ms, Memory: 222 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,15 +2,24 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
-        
-        for(int i=0;i<phone_book.length-1;i++){
-            if(phone_book[i+1].startsWith(phone_book[i])){
-                return false;
+
+        Set<String> set = new HashSet<>();
+
+        for(String phone : phone_book) {
+            set.add(phone);
+        }
+
+        for(String phone : phone_book) {
+
+            for(int i = 1; i < phone.length(); i++) {
+                String prefix = phone.substring(0, i);
+
+                if(set.contains(prefix)) {
+                    return false;
+                }
             }
         }
+
         return true;
-        
-        
     }
 }
